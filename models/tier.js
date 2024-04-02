9654329722de052601f30cdb2f64edc819b9a31c
AbeGue02 <abrahamgue02@gmail.com>
2024-04-02 16:52:39 -0400

    Updates Tier Schema

--- models/tier.js
+++ models/tier.js
@@ -4,8 +4,8 @@ const TierSchema = new Schema(
     {
         name: { type: String, required: true },
         emoji: { type: String, required: true },
-        min_score: { type: Number, required: true },
-        max_score: { type: Number, required: true },
+        min_score: { type: Number, required: true, min: 0 },
+        max_score: { type: Number },
     },
     { timestamps: true },
 )
